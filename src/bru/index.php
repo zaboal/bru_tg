84249cfb3c4c9b4fc1f49a587c0ef1ee1cc78cd6
Bogdan Zažigin <me@zba.su>
2025-02-10 23:43:40 +0000

    Добавить комментарии к функциям с помощью ИИ

--- src/bru/index.php
+++ src/bru/index.php
@@ -1,65 +1,84 @@
 <?php
 
+/**
+ * Отправляет сообщение в указанный чат Telegram.
+ *
+ * @param int $chat Идентификатор чата, в который нужно отправить сообщение.
+ * @param string $text Текст сообщения для отправки.
+ */
 function sendMessage($chat, $text)
 {
-	$data = [
-		'chat_id' => $chat,
-		'text' => $text
-	];
-	$api = $_ENV['API_KEY'];
-	$url = "https://api.telegram.org/bot$api/sendMessage";
-	$ch = curl_init();
+    $data = [
+        'chat_id' => $chat,
+        'text' => $text
+    ];
+    $api = $_ENV['API_KEY'];
+    $url = "https://api.telegram.org/bot$api/sendMessage";
+    $ch = curl_init();
 
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	curl_exec($ch);
-	curl_close($ch);
+    curl_exec($ch);
+    curl_close($ch);
 }
 
+/**
+ * Получает идентификатор чата Telegram, связанный с указанным номером телефона.
+ *
+ * @param string $number Номер телефона для поиска.
+ * @return int|false Идентификатор чата Telegram, если найден, иначе false.
+ */
 function getChat($number)
 {
-	$url = 'https://api.us-east.aws.tinybird.co/v0/pipes/untitled_pipe_7865.json';
-	$params = ['num' => $number];
-	$token = $_ENV['TOKEN'];
-	$ch = curl_init();
-	curl_setopt_array($ch, [
-		CURLOPT_URL => $url . '?' . http_build_query($params),
-		CURLOPT_HTTPHEADER => [
-			'Authorization: Bearer ' . $token,
-			'Accept-Encoding: gzip'
-		],
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_TIMEOUT => 30
-	]);
+    $url = 'https://api.us-east.aws.tinybird.co/v0/pipes/untitled_pipe_7865.json';
+    $params = ['num' => $number];
+    $token = $_ENV['TOKEN'];
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url . '?' . http_build_query($params),
+        CURLOPT_HTTPHEADER => [
+            'Authorization: Bearer ' . $token,
+            'Accept-Encoding: gzip'
+        ],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_TIMEOUT => 30
+    ]);
 
-	$response = curl_exec($ch);
+    $response = curl_exec($ch);
 
-	$response = json_decode(gzdecode($response));
-	curl_close($ch);
-	if (! $response->data[0]->tg) {
-		return false;
-	}
-	return (int)$response->data[0]->tg;
+    $response = json_decode(gzdecode($response));
+    curl_close($ch);
+    if (! $response->data[0]->tg) {
+        return false;
+    }
+    return (int)$response->data[0]->tg;
 }
 
+/**
+ * Обрабатывает входящие события и отправляет сообщение пользователю.
+ *
+ * @param array $event Данные события.
+ * @param array $context Данные контекста (не используются).
+ * @throws Exception Если тело события отсутствует.
+ */
 function handler($event, $context)
 {
-	# context - useless, $event - encoded data
-	if (! $event['body']) {
-		throw new Exception("No body in mesage");
-	}
+    // context - бесполезен, $event - закодированные данные
+    if (! $event['body']) {
+        throw new Exception("No body in message");
+    }
 
-	$текст = base64_decode($event['body'], true);
-	parse_str($текст, $params);
-	$changes = json_decode($params['changes'], true);
-	$data = json_decode($params['data']);
-	$new = $changes[1]['data']['bonus_sum'];
-	$delta = $new - $changes[0]['data']['bonus_sum'];
-	$text = "Благодарим за покупку!\nНачислено " . $delta . " баллов, теперь у Вас " . $new . " баллов";
-	$id = getChat($data->num);
-	sendMessage($id, $text);
+    $text = base64_decode($event['body'], true);
+    parse_str($text, $params);
+    $changes = json_decode($params['changes'], true);
+    $data = json_decode($params['data']);
+    $new = $changes[1]['data']['bonus_sum'];
+    $delta = $new - $changes[0]['data']['bonus_sum'];
+    $message = "Благодарим за покупку!\nНачислено " . $delta . " баллов, теперь у Вас " . $new . " баллов";
+    $id = getChat($data->num);
+    sendMessage($id, $message);
 }
--- src/functional/log.php
+++ src/functional/log.php
@@ -1,71 +1,84 @@
 <?php
 
+/**
+ * Логирует входящее сообщение в Tinybird API.
+ *
+ * @param string $incoming_message Входящее сообщение в формате JSON.
+ * @return bool True, если сообщение было успешно залогировано, false в противном случае.
+ */
 function log_message($incoming_message): bool
 {
-	$data = json_decode($incoming_message, true);
+    $data = json_decode($incoming_message, true);
 
-	if ($data === null) {
-		return false;
-	}
+    if ($data === null) {
+        return false;
+    }
 
-	$json_data = json_encode(array("data" => json_encode($data)));
+    $json_data = json_encode(array("data" => json_encode($data)));
 
-	if ($json_data === false) {
-		return false;
-	}
+    if ($json_data === false) {
+        return false;
+    }
 
-	$curl = curl_init();
+    $curl = curl_init();
 
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://api.us-east.aws.tinybird.co/v0/events?name=schema_ds_7565",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "POST",
-		CURLOPT_POSTFIELDS => $json_data,
-		CURLOPT_HTTPHEADER => array(
-			"Authorization: Bearer " . $_ENV["TOKEN"],
-			"Content-Type: application/json"
-		),
-	));
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.us-east.aws.tinybird.co/v0/events?name=schema_ds_7565",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $json_data,
+        CURLOPT_HTTPHEADER => array(
+            "Authorization: Bearer " . $_ENV["TOKEN"],
+            "Content-Type: application/json"
+        ),
+    ));
 
-	curl_exec($curl);
-	return true;
+    curl_exec($curl);
+    return true;
 }
 
+/**
+ * Логирует пользователя, отправляя его Telegram ID и номер телефона в Tinybird API.
+ *
+ * @param string $tg Telegram ID пользователя.
+ * @param int $number Номер телефона пользователя.
+ * @return bool True, если пользователь был успешно залогирован, false в противном случае.
+ */
 function login_user($tg, $number): bool
 {
-	$data = [
-		'num' => (int)$number,
-		'tg' => $tg
-	];
+    $data = [
+        'num' => (int)$number,
+        'tg' => $tg
+    ];
 
-	$json_data = json_encode($data);
+    $json_data = json_encode($data);
 
-	$curl = curl_init();
+    $curl = curl_init();
 
-	$token = $_ENV["TOKEN2"];
+    $token = $_ENV["TOKEN2"];
 
-	curl_setopt_array($curl, array(
-		CURLOPT_URL => "https://api.us-east.aws.tinybird.co/v0/events?name=demo_custom_data_2453",
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "POST",
-		CURLOPT_POSTFIELDS => $json_data,
-		CURLOPT_HTTPHEADER => array(
-			"Authorization: Bearer $token",
-			"Content-Type: application/json"
-		),
-	));
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.us-east.aws.tinybird.co/v0/events?name=demo_custom_data_2453",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $json_data,
+        CURLOPT_HTTPHEADER => array(
+            "Authorization: Bearer $token",
+            "Content-Type: application/json"
+        ),
+    ));
 
-	$response = curl_exec($curl);
-	echo $response;
-	curl_close($curl);
-	$ppp = json_decode($response);
-	return $ppp->successful_rows == 1;
+    $response = curl_exec($curl);
+    echo $response;
+    curl_close($curl);
+    $ppp = json_decode($response);
+    return $ppp->successful_rows == 1;
 }
